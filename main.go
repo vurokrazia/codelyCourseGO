902e5745234803d2ffacbdd0ed99ceaf1f0726d9
Jesus <jesus.alberto.vk@gmail.com>
2022-03-15 21:16:36 -0600

    update methods

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var beers = map[string]string{
+	"x": "4",
+	"f": "3",
+	"a": "1",
+	"g": "2",
+}
 
 func main() {
+	b := flag.Bool("beers", false, "print beers")
+	flag.Parse()
+	if *b {
+		fmt.Println(beers)
+	}
 	fmt.Printf("hello")
 }
